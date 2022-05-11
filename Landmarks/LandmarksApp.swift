9181b7e5ef04eaac2c585ca49ae401ff9f76199e
honkhonkgoose <honkhonkgoose@users.noreply.github.com>
2022-05-10 22:39:10 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Anastasia Sullivan on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
